c400723a624c2a1778af22642744c6d4692d435c
xachman <zironside@hotmail.com>
2020-03-24 01:15:30 +0000

    add logging

--- simple-task-scheduler.py
+++ simple-task-scheduler.py
@@ -1,7 +1,11 @@
 import argparse
 import configparser
 import os
+import subprocess
 import io
+import datetime
+
+now = datetime.datetime.now()
 
 parser = argparse.ArgumentParser(description='Simple Task Scheduler')
 parser.add_argument('time', type=str,
@@ -9,14 +13,28 @@ parser.add_argument('time', type=str,
 parser.add_argument('-d', '--directory', type=str,
                     help='directory that configs are loaded from')
 
+parser.add_argument('-l', '--log', type=str,
+                    help='log file to output logs to')
 args = parser.parse_args()
 
 task_time = args.time
 start_directory = args.directory
+log_file = args.log
 
 if start_directory is None:
-    start_directory = "/etc/task-scheduler"
+    start_directory = "/etc/simple-task-scheduler"
 
+if log_file is None:
+    log_file = '/var/simple-task-scheduler.log'
+
+def log(type, message):
+    f = open(log_file, 'a+')
+    l_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    for line in message.splitlines():
+        if line.strip() == "":
+            continue
+        f.write('['+type+' '+l_time+'] '+line+"\n")
+    f.close()
 
 
 directory = start_directory+"/conf.d"
@@ -26,5 +44,11 @@ for filename in os.listdir(directory):
         config.read(directory+"/"+filename)
 
         if config.get('task', 'time') == task_time:
-            os.system(config.get('task', 'command'))
-        
+            result = subprocess.run(config.get('task', 'command').split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if result.stderr:
+                log('error', result.stderr.decode('utf-8'))
+            else:
+                log('info', result.stdout.decode('utf-8'))
+
+
+
